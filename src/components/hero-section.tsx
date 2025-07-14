11345be901f0ca1e4500a141a88f04c9480e5cf4
AlaaHamdy <alaahamdy2197@gmail.com>
2025-07-15 00:01:36 +0300

    style: 🎨 Improve home page, diploma details 🎨

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { motion } from "framer-motion";
@@ -7,23 +8,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+import HeroFrame from "@/app/assets/Section.png";
+
 export default function HeroSection() {
   const locale = useLocale();
   const t = useTranslations("hero");
 
   return (
-    <section className="relative pt-32 pb-16 px-4 min-h-[100vh] flex flex-col xl:flex-row justify-center items-center bg-white text-white overflow-hidden">
-      {/* Animated Background Elements */}
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
-        <div className="grid lg:grid-cols-2 gap-12 items-center">
-          {/* Animated Text */}
+    <section
+      className="relative pt-32 pb-16 px-4 min-h-[100vh] flex flex-col xl:flex-row justify-center items-center bg-white text-white overflow-hidden"
+      style={{ backgroundImage: `url(${HeroFrame.src})` }}
+    >
+      <div className="container h-full mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
+        <div className="h-full grid lg:grid-cols-2 gap-12 items-center ">
           <motion.div
             initial={{ x: -200, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
-            className="flex flex-col gap-6"
+            className="flex flex-col justify-between h-full"
           >
-            <h1 className="text-[32px] md:text-[60px] font-bold text-main-black">
+            <h1 className="text-[32px] md:text-[60px] leading-[1.4] font-bold text-main-black">
               {t("Learn develop and achieve anytime anywhere")}
             </h1>
             <p className="text-xl sm:text-[32px] leading-[1.5] text-black-tint-70">
@@ -37,21 +41,22 @@ export default function HeroSection() {
             </Link>
           </motion.div>
 
-          {/* Animated Image */}
           <motion.div
             initial={{ x: 200, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
-            className="relative z-10"
+            className="relative h-full w-full z-10 flex justify-center items-center"
           >
-            <Image
-              src="/hero-frame.svg"
-              alt="Students learning online"
-              className="w-full h-auto"
-              width={300}
-              height={200}
-              priority
-            />
+            <div className="relative h-[60vh] w-auto">
+              <Image
+                src="/heroFrame2.svg"
+                alt="Students learning online"
+                className="shadow-[-4px_4px_15.8px_10px_rgba(60,67,91,0.10)]"
+                fill
+                priority
+                style={{ objectFit: "contain" }}
+              />
+            </div>
           </motion.div>
         </div>
       </div>
