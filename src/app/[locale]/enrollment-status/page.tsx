da860d9b925b65f67a28be1f292a20441fbdcb58
AlaaHamdy <alaahamdy2197@gmail.com>
2025-07-18 16:54:51 +0300

    ✨ Implement generate metadata comp to improve seo

--- src/app/[locale]/enrollment-status/page.tsx
+++ src/app/[locale]/enrollment-status/page.tsx
@@ -1,33 +1,30 @@
 import { fetchStatus } from "@/app/api/enrollmentStatus/enrollmentService";
-import Seo from "@/components/Seo";
-import { cookies } from "next/headers";
 import Pending from "./Pending";
 // import Approved from "./Approved";
 // import Rejected from "./Rejected";
+import { generatePageMetadata } from "@/lib/metadata";
+
+export async function generateMetadata() {
+  return await generatePageMetadata({
+    ar: {
+      title: "حالة التسجيل - الجامعة الإلكترونية السعودية - الكلية التطبيقية",
+      description:
+        "تفقد حالة تسجيلك في الجامعة الإلكترونية السعودية - الكلية التطبيقية.",
+    },
+    en: {
+      title:
+        "Enrollment Status - Saudi Electronic University - College of Applied Studies",
+      description:
+        "Check your enrollment status at Saudi Electronic University - College of Applied Studies.",
+    },
+  });
+}
 
 export default async function page() {
   const enrollmentData = await fetchStatus();
-  const cookieStore = cookies();
-  const lang = cookieStore.get("NEXT_LOCALE")?.value || "en";
-
-  const seoContent =
-    lang === "ar"
-      ? {
-          title:
-            "حالة التسجيل - الجامعة الإلكترونية السعودية - الكلية التطبيقية",
-          description:
-            "تفقد حالة تسجيلك في الجامعة الإلكترونية السعودية - الكلية التطبيقية.",
-        }
-      : {
-          title:
-            "Enrollment Status - Saudi Electronic University - College of Applied Studies",
-          description:
-            "Check your enrollment status at Saudi Electronic University - College of Applied Studies.",
-        };
 
   return (
     <>
-      <Seo {...seoContent} />
       <Pending enrollmentData={enrollmentData} />
       {/* <Approved enrollmentData={enrollmentData} /> */}
       {/* <Rejected enrollmentData={enrollmentData} /> */}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,32 +4,30 @@ import AboutSection from "@/components/about-section";
 import DiplomasSection from "@/components/diplomas-section";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/hero-section";
-import Seo from "@/components/Seo";
-import { cookies } from "next/headers";
+import { generatePageMetadata } from "@/lib/metadata";
+
+export async function generateMetadata() {
+  return await generatePageMetadata({
+    ar: {
+      title: "الجامعة الإلكترونية السعودية - الكلية التطبيقية",
+      description:
+        "الموقع منصة إلكترونية مبتكرة تُقدم تجربة تعليمية شاملة للمتعلمين باللغتين العربية والإنجليزية. استكشف مجموعة واسعة من الشهادات والموارد والأدوات التفاعلية المصممة لدعم الطلاب والمعلمين في تحقيق التميز الأكاديمي.",
+      image: "/assets/artificialIntelligence.jpg",
+    },
+    en: {
+      title: "Saudi Electronic University - College of Applied Studies",
+      description:
+        "The website is an innovative online platform offering a comprehensive educational experience for learners in both Arabic and English. Explore a wide range of diplomas, resources, and interactive tools designed to support students and educators in achieving academic excellence.",
+      image: "/assets/artificialIntelligence.jpg",
+    },
+  });
+}
 
 export default async function HomePage() {
   const diplomasData = await fetchDiplomas();
-  const cookieStore = cookies();
-  const lang = cookieStore.get("NEXT_LOCALE")?.value || "en";
-
-  const seoContent =
-    lang === "ar"
-      ? {
-          title: "الجامعة الإلكترونية السعودية - الكلية التطبيقية",
-          description:
-            "الموقع منصة إلكترونية مبتكرة تُقدم تجربة تعليمية شاملة للمتعلمين باللغتين العربية والإنجليزية. استكشف مجموعة واسعة من الشهادات والموارد والأدوات التفاعلية المصممة لدعم الطلاب والمعلمين في تحقيق التميز الأكاديمي.",
-          image: "../assets/artificialIntelligence.jpg",
-        }
-      : {
-          title: "Saudi Electronic University - College of Applied Studies",
-          description:
-            "The website is an innovative online platform offering a comprehensive educational experience for learners in both Arabic and English. Explore a wide range of diplomas, resources, and interactive tools designed to support students and educators in achieving academic excellence.",
-          image: "../assets/artificialIntelligence.jpg",
-        };
 
   return (
     <div className="min-h-screen bg-white">
-      <Seo {...seoContent} />
       <main
         style={{
           backgroundImage: `url(${HeroFrame.src})`,
